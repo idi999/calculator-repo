5aa1e4b830f1dac2f62fa5c76859f80da43a3505
CRA555 <40105142@hughbaird.ac.uk>
2024-10-07 11:42:35 +0100

    Update calculator.py
    This is a updated version, if there are problems please let me know. 
    Callum R-A

--- calculator.py
+++ calculator.py
@@ -8,24 +8,24 @@ try:
     return a * b
   def divide(a, b):
     return a / b
-    a = float(input('Would you kindly input a number. '))
-    b = float(input('Would you kindly input a number. '))
-    operator = input('Would you kindly pick a operator, which could be +, -, / or *. ')
-    if operator == '+':
-      result = add(a, b)
-    elif operator == '-':
-      result = subtract(a, b)
-    elif operator == '*':
-      result = multiply(a, b)
-    elif operator == '/':
-      result = divide(a, b)
-    else:
-      print('You have entered a value that is not a symbol so the calcltor has stopped.')
-      exit()
-    print('This is your answer:', result)
+  a = float(input('Would you kindly input a number. '))
+  b = float(input('Would you kindly input a number. '))
+  operator = input('Would you kindly pick a operator, which could be +, -, / or *. ')
+  if operator == '+':
+    result = add(a, b)
+  elif operator == '-':
+    result = subtract(a, b)
+  elif operator == '*':
+    result = multiply(a, b)
+  elif operator == '/':
+    result = divide(a, b)
+  else:
+    print('You have entered a value that is not a symbol so the calcltor has stopped.')
+    exit()
+  print('This is your answer:', result)
 except ZeroDivisionError:
   print('You cannot divde by zero.')
-except VauleError:
+except ValueError:
   print('You have entered a invaild data type.')
 finally:
   print('The code is complete its run.')
