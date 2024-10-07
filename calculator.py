db21733a9fdfc236b055d56e172c19d24683da91
CRA555 <40105142@hughbaird.ac.uk>
2024-10-07 11:31:11 +0100

    Update calculator.py
    This has has a updated copy of the code in with more options and error checks for anything that would break the code, if there are any issues please let me know and leave a comment in the code to show where the problem is. 
    Callum Rynhart-Albert

--- calculator.py
+++ calculator.py
@@ -1,2 +1,32 @@
-def add(x, y):
-  return x+y
+import math 
+try: 
+  def add(a, b):
+    return a + b
+  def subtract(a, b):
+    return a - b
+  def multiply(a, b):
+    return a * b
+  def divide(a, b):
+    return a / b
+    a = float(input('Would you kindly input a number. '))
+    b = float(input('Would you kindly input a number. '))
+    operator = input('Would you kindly pick a operator, which could be +, -, / or *. ')
+    if operator == '+':
+      result = add(a, b)
+    elif operator == '-':
+      result = subtract(a, b)
+    elif operator == '*':
+      result = multiply(a, b)
+    elif operator == '/':
+      result = divide(a, b)
+    else:
+      print('You have entered a value that is not a symbol so the calcltor has stopped.')
+      exit()
+    print('This is your answer:', result)
+except ZeroDivisionError:
+  print('You cannot divde by zero.')
+except VauleError:
+  print('You have entered a invaild data type.')
+finally:
+  print('The code is complete its run.')
+      
